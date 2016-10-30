933a1c20ea3c1563e9c112da57b494588c9ffb8d
marquesm91 <marquesm91@gmail.com>
2016-10-30 14:46:52 -0200

    v1.2 Adicionado a possibilidade de gerar n sorteios. Ao inves de vetor lista_numeros agora é uma estrutura para armazenar mais informações. Ordenação de vetores

--- README.md
+++ README.md
@@ -7,10 +7,10 @@ Gerar dados estatísticos para o jogo Mega Sena.
 Criar um programa na linguagem C sobre a MEGASENA com as seguintes funções:
 
 * ~~Gerar numeros aleatorios entre 1 e 60.~~
-* Gerar 3000 sorteios realmente aleatórios, em uma matriz, cada um com 6 números únicos (01 a 60).
-* Exibir os números por ordem do sorteio.
-* Exibir os números de cada sorteio em ordem crescente.
-* Gerar uma lista de estatística com os seguintes dados: a) Quantidade de vezes que cada número saiu nos sorteios (Ordem crescente). Por exemplo: Numero 05 = 290 vezes.
+* ~~Gerar 3000 sorteios realmente aleatórios, em uma matriz, cada um com 6 números únicos (01 a 60).~~
+* ~~Exibir os números por ordem do sorteio.~~
+* ~~Exibir os números de cada sorteio em ordem crescente.~~
+* ~~Gerar uma lista de estatística com os seguintes dados: a) Quantidade de vezes que cada número saiu nos sorteios (Ordem crescente). Por exemplo: Numero 05 = 290 vezes.~~
 * Listar as 15 duplas que mais saíram nos sorteios. Por exemplo: Números 03 e 29 = 156 vezes.
 * Listar os 15 números únicos que mais saíram nos sorteios (Ordem crescente). Por exemplo: Numero 02 = 360 vezes.
 * Listar de 01 a 60 a quantidade de jogadas desde a ultima vez que o número foi sorteado. Por exemplo: Nº 01 = não sai a 7 sorteios seguidos. Nº 02 = não sai a 15 sorteios seguidos. Nº 03 = saiu no ultimo sorteio. Nº 04 = não sai a 31 sorteios seguidos Nº 05 = não sai a 3 sorteios seguidos .... Até o Nº 60.
--- main.c
+++ main.c
@@ -2,46 +2,128 @@
 #include <stdlib.h>
 #include <time.h>
 
-int geraNumero(int inicio, int final) {
-	int random_number = rand(); // entre 1 e RAND_MAX (constante stdlib.h)
+#define TAM_SORTEIO 6
+#define TOTAL_NUM 60
+#define TOTAL_SORTEIO 3000
 
-	// Alterando o range para [1,final) de random_number
-	random_number = random_number % final;  // [1,60)
+struct Lista_numeros{
+	int numero;
+	int ocorrencias; // Quantas vezes ele apareceu nos sorteios
+	int ultimo_sorteio; // Ultima vez que apareceu nos sorteios
+};
 
-	// Alterando o range para [inicio + 1, inicio + final) ou [inicio + 1, inicio + final - 1] de random_number 
-	random_number = random_number + inicio - 1; // [1,60]
+void imprime_sorteio(int sorteio[]) {
+	for (int j = 0; j < TAM_SORTEIO; j++){
+		if(j < TAM_SORTEIO - 1)
+			printf("%d - ", sorteio[j]);
+		else
+			printf("%d", sorteio[j]);
+	}
+	printf("\n");
+}
 
-	return random_number;
+// Ordena numeros do sorteio na ordem crescente
+void ordena_sorteio(int sorteio[]){   
+	for(int i = 0; i < TAM_SORTEIO; i++){ 
+		for(int j = i + 1; j < TAM_SORTEIO; j++) {
+			if(sorteio[i] > sorteio[j]){ 
+				int aux = sorteio[i]; 
+				sorteio[i] = sorteio[j]; 
+				sorteio[j] = aux; 
+      		}
+		}
+  	} 
+}
+
+// Ordena lista baseado nas ocorrencias de cada numero na ordem crescente
+void ordena_lista(struct Lista_numeros lista_numeros[]) {
+	for(int i = 0; i < TOTAL_NUM; i++){ 
+		for(int j = i + 1; j < TOTAL_NUM; j++) {
+			if(lista_numeros[i].ocorrencias > lista_numeros[j].ocorrencias){ 
+				struct Lista_numeros aux = lista_numeros[i]; 
+				lista_numeros[i] = lista_numeros[j]; 
+				lista_numeros[j] = aux; 
+      		}
+		}
+  	}
+}
+
+
+int gera_numero(int inicio, int final) {
+	int numero = rand(); // entre 0 e RAND_MAX (constante stdlib.h)
+
+	// Alterando o range para [0,final) de numero
+	numero = numero % final;  // [1,60)
+
+	// Alterando o range para [inicio, inicio + final) ou [inicio, inicio + final - 1] de numero 
+	numero = numero + inicio; // [1,61) ou [1,60]
+
+	return numero;
+}
+
+int gera_sorteio(int inicio, int final, int sorteio[], struct Lista_numeros lista_numeros[]){
+	
+	int numero = gera_numero(inicio, final);
+	sorteio[0] = numero;
+
+	// Gerar TAM_SORTEIO numeros sempre diferentes
+	for(int i = 1; i < TAM_SORTEIO; i++){
+		sorteio[i] = gera_numero(inicio, final);
+		for(int j = i - 1; j >= 0; j--) {
+			if (sorteio[j] == sorteio[i]) {
+				sorteio[i] = gera_numero(inicio, final);
+				j = i - 1;
+			}
+		}
+		lista_numeros[sorteio[i]-1].ocorrencias++;
+	}
+	return 0;
 }
 
 int main ()
 {
-	FILE *f = fopen("resultados.csv", "w+t");
+	FILE *arquivo = fopen("resultados.csv", "w+t");
+
+	if (!arquivo) {
+		printf("Nao foi possivel abrir o arquivo resultados.csv para escrita.");
+		exit(-1);
+	}
 
 	srand (time(NULL)); // Gerando seed para numeros randomicos
-
-	int listaNumeros[60];
+	struct Lista_numeros lista_numeros[TOTAL_NUM];
+	int sorteios[TOTAL_SORTEIO][TAM_SORTEIO]; // TOTAL_SORTEIO sorteios de 6 numeros cada um
 	int inicio = 1;
-	int final = 60;
+	int final = TOTAL_NUM;
 
-	// Zerando valores do vetor
-	for (int i = 0; i < 60; i++){
-		listaNumeros[i] = 0;
+	// Zerando valores da estrutura Lista_numeros
+	for (int i = 0; i < TOTAL_NUM; i++){
+		lista_numeros[i].numero = i + 1;
+		lista_numeros[i].ocorrencias = 0;
+		lista_numeros[i].ultimo_sorteio = 0;
+	}
+
+	// Zerando valores da estrutura Lista_numeros
+	for(int i = 0; i < TOTAL_SORTEIO; i++){
+		for (int j = 0; j < TAM_SORTEIO; j++){
+			sorteios[i][j] = 0;
+		}
 	}
 
 	// Gerando 3000 numeros aleatorios entre 1 e 60 e salvando suas ocorrencias
-	for (int i = 0; i < 3000; i++){
-		int index = geraNumero(inicio, final);
-		listaNumeros[index]++;
+	for (int i = 0; i < TOTAL_SORTEIO; i++){
+		gera_sorteio(inicio, final, sorteios[i], lista_numeros);
+		ordena_sorteio(sorteios[i]);
+		printf("Sorteio[%d] = ", i+1); imprime_sorteio(sorteios[i]);
 	}
 
-	// Imprimindo resultados
-	fprintf(f,"NUMERO;OCORRENCIAS\n");
-	for (int i = 0; i < 60; i++){
-		printf("o numero %d foi sorteado %d vezes\n", i+1, listaNumeros[i]);
-		fprintf(f,"%d;%d\n", i+1, listaNumeros[i]);
+	// Imprimindo ocorrencias de cada numero de maneira ordenada
+	ordena_lista(lista_numeros);
+	fprintf(arquivo,"NUMERO;OCORRENCIAS\n");
+	for (int i = 0; i < TOTAL_NUM; i++){
+		printf("o numero %d foi sorteado %d vezes\n", lista_numeros[i].numero, lista_numeros[i].ocorrencias);
+		fprintf(arquivo,"%d;%d\n", lista_numeros[i].numero, lista_numeros[i].ocorrencias);
 	}
 
-	fclose(f);
+	fclose(arquivo);
 	return 0;
 }
